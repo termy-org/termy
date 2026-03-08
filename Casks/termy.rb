25bbe052902cbb0be10b8fdee696732db83dfe4d
lasse <77295879+lassejlv@users.noreply.github.com>
2026-03-08 15:28:24 +0100

    Update Termy to version 0.1.47 with new SHA256

--- Casks/termy.rb
+++ Casks/termy.rb
@@ -1,9 +1,9 @@
 cask "termy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.46"
-  sha256 arm:   "9e909a8e8ac3e6e3c820bcf8e571756825b1579f221b418a434101c86720dcaf",
-         intel: "46d3ef42a5781f808d9bc7b60dd1c0ed37e8dd40de0eb4ddf0b8afae1fdc8b90"
+  version "0.1.47"
+  sha256 arm:   "44baf06968d0cd044173efcc18f61b2e7a356e96657e2e6e4b3e74be436740e4",
+         intel: "92a9f6e27fb25740d7e8a7a6b6735bf0958b0fec251eda27dcc40531de4f05c0"
 
   url "https://github.com/lassejlv/termy/releases/download/v#{version}/Termy-v#{version}-macos-#{arch}.dmg"
   name "Termy"
