fde0fcea8c1cf6e0f6d4ff46decc4372c02da7eb
lasse <77295879+lassejlv@users.noreply.github.com>
2026-03-23 17:08:26 +0100

    ci: update homebrew cask workflow to push to homebrew-termy repo

--- Casks/termy.rb
+++ Casks/termy.rb
@@ -5,10 +5,10 @@ cask "termy" do
   sha256 arm:   "9289be9951a5afc7d2c446eb0bb00730c4451f17c6792a10c6d08019cbbdcf47",
          intel: "0e3cd12d5f2712207d378a4519607b81087d66341fb1225869ee0ccc883e7a8d"
 
-  url "https://github.com/lassejlv/termy/releases/download/v#{version}/Termy-v#{version}-macos-#{arch}.dmg"
+  url "https://github.com/termy-org/termy/releases/download/v#{version}/Termy-v#{version}-macos-#{arch}.dmg"
   name "Termy"
   desc "Minimal GPU-powered terminal written in Rust"
-  homepage "https://github.com/lassejlv/termy"
+  homepage "https://termy.dev"
 
   livecheck do
     url :url
