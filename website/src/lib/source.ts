10865c458fa75e45c7760bb9b3c22e1fa98c2a8f
Lasse <77295879+lassejlv@users.noreply.github.com>
2026-05-13 09:13:28 +0200

    Website changes

--- website/src/lib/source.ts
+++ website/src/lib/source.ts
@@ -1,12 +1,10 @@
 import { loader } from 'fumadocs-core/source';
 import { docs } from 'collections/server';
-import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
 import { docsRoute } from './shared';
 
 export const source = loader({
   source: docs.toFumadocsSource(),
   baseUrl: docsRoute,
-  plugins: [lucideIconsPlugin()],
 });
 
 export function markdownPathToSlugs(segs: string[]) {
